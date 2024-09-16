0975e938a5bee26159871102a270a878a3a9552c
Loc Nguyen Hoang <nhloc08@gmail.com>
2024-09-16 11:36:09 +0700

    final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,38 +36,37 @@
             clearButton = new Button();
             backgroundButton = new Button();
             closeButton = new Button();
+            pictureBox1 = new PictureBox();
             openFileDialog1 = new OpenFileDialog();
             colorDialog1 = new ColorDialog();
-            panel1 = new Panel();
-            pictureBox1 = new PictureBox();
             tableLayoutPanel1.SuspendLayout();
             flowLayoutPanel1.SuspendLayout();
-            panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.AutoScroll = true;
+            tableLayoutPanel1.AutoSize = true;
             tableLayoutPanel1.ColumnCount = 2;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85F));
             tableLayoutPanel1.Controls.Add(checkBox1, 0, 1);
             tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 1, 1);
-            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 2;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
-            tableLayoutPanel1.Size = new Size(843, 339);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 88.75F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 11.25F));
+            tableLayoutPanel1.Size = new Size(850, 393);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(3, 308);
+            checkBox1.Location = new Point(3, 351);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(80, 25);
             checkBox1.TabIndex = 1;
@@ -83,15 +82,15 @@
             flowLayoutPanel1.Controls.Add(closeButton);
             flowLayoutPanel1.Dock = DockStyle.Fill;
             flowLayoutPanel1.FlowDirection = FlowDirection.RightToLeft;
-            flowLayoutPanel1.Location = new Point(129, 308);
+            flowLayoutPanel1.Location = new Point(130, 351);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(711, 28);
+            flowLayoutPanel1.Size = new Size(717, 39);
             flowLayoutPanel1.TabIndex = 2;
             // 
             // showButton
             // 
             showButton.AutoSize = true;
-            showButton.Location = new Point(585, 3);
+            showButton.Location = new Point(591, 3);
             showButton.Name = "showButton";
             showButton.Size = new Size(123, 31);
             showButton.TabIndex = 0;
@@ -102,7 +101,7 @@
             // clearButton
             // 
             clearButton.AutoSize = true;
-            clearButton.Location = new Point(445, 3);
+            clearButton.Location = new Point(451, 3);
             clearButton.Name = "clearButton";
             clearButton.Size = new Size(134, 31);
             clearButton.TabIndex = 1;
@@ -113,7 +112,7 @@
             // backgroundButton
             // 
             backgroundButton.AutoSize = true;
-            backgroundButton.Location = new Point(245, 3);
+            backgroundButton.Location = new Point(251, 3);
             backgroundButton.Name = "backgroundButton";
             backgroundButton.Size = new Size(194, 31);
             backgroundButton.TabIndex = 2;
@@ -124,7 +123,7 @@
             // closeButton
             // 
             closeButton.AutoSize = true;
-            closeButton.Location = new Point(141, 3);
+            closeButton.Location = new Point(147, 3);
             closeButton.Name = "closeButton";
             closeButton.Size = new Size(98, 31);
             closeButton.TabIndex = 3;
@@ -132,39 +131,28 @@
             closeButton.UseVisualStyleBackColor = true;
             closeButton.Click += closeButton_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            tableLayoutPanel1.SetColumnSpan(pictureBox1, 2);
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(844, 342);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
             // openFileDialog1
             // 
             openFileDialog1.FileName = "openFileDialog1";
             openFileDialog1.Filter = resources.GetString("openFileDialog1.Filter");
             openFileDialog1.Title = "Select a picture file.";
             // 
-            // panel1
-            // 
-            panel1.AutoScroll = true;
-            tableLayoutPanel1.SetColumnSpan(panel1, 2);
-            panel1.Controls.Add(pictureBox1);
-            panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(3, 3);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(837, 299);
-            panel1.TabIndex = 3;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox1.Dock = DockStyle.Fill;
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(837, 299);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            AutoSize = true;
-            ClientSize = new Size(843, 339);
+            ClientSize = new Size(850, 393);
             Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
@@ -172,9 +160,9 @@
             tableLayoutPanel1.PerformLayout();
             flowLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.PerformLayout();
-            panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -188,7 +176,6 @@
         private Button closeButton;
         private OpenFileDialog openFileDialog1;
         private ColorDialog colorDialog1;
-        private Panel panel1;
         private PictureBox pictureBox1;
     }
 }
